a6e4211033027407f8b9e66de0991ffa502c1c10
uttu87 <haivan.qn@gmail.com>
2019-05-02 22:11:25 +0700

    Implement group channel tabs

--- app/src/main/java/com/iseasoft/iseaiptv/ui/activity/MainActivity.java
+++ app/src/main/java/com/iseasoft/iseaiptv/ui/activity/MainActivity.java
@@ -1,26 +1,74 @@
 package com.iseasoft.iseaiptv.ui.activity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.iseasoft.iseaiptv.R;
+import com.iseasoft.iseaiptv.adapters.ChannelAdapter;
 import com.iseasoft.iseaiptv.helpers.Router;
+import com.iseasoft.iseaiptv.http.HttpHandler;
+import com.iseasoft.iseaiptv.models.M3UItem;
+import com.iseasoft.iseaiptv.models.M3UPlaylist;
+import com.iseasoft.iseaiptv.models.Playlist;
+import com.iseasoft.iseaiptv.parsers.M3UParser;
+import com.iseasoft.iseaiptv.permissions.IseaSoft;
+import com.iseasoft.iseaiptv.permissions.PermissionCallback;
 import com.iseasoft.iseaiptv.ui.fragment.ChannelFragment;
+import com.iseasoft.iseaiptv.utils.PreferencesUtility;
+import com.iseasoft.iseaiptv.utils.Utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout panelLayout;
+
+    private ChannelAdapter channelAdapter;
+    private M3UPlaylist mPlaylist;
+    private ViewPager viewPager;
+    private final PermissionCallback permissionReadstorageCallback = new PermissionCallback() {
+        @Override
+        public void permissionGranted() {
+            loadChannels();
+        }
+
+        @Override
+        public void permissionRefused() {
+
+        }
+    };
+
+    public M3UPlaylist getPlaylist() {
+        return mPlaylist;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +76,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        setupChannel();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,16 +94,70 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        panelLayout = findViewById(R.id.drawer_layout);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        if (Utils.isMarshmallow()) {
+            requestStoragePermission();
+        } else {
+            loadChannels();
+        }
     }
 
-    private void setupChannel() {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
+    private void loadChannels() {
+        final Playlist lastPlaylist = PreferencesUtility.getInstance(this).getLastPlaylist();
+        if (lastPlaylist != null) {
+            if (lastPlaylist.getLink().startsWith("http")) {
+                loadServer(lastPlaylist.getLink());
+            } else {
+                try {
+                    File file = new File(lastPlaylist.getLink());
+                    InputStream inputStream = new FileInputStream(file);
+                    parseAndUpdateUI(inputStream);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            showChannelPlaceholder();
+            Router.navigateTo(this, Router.Screens.PLAYLIST);
+        }
 
-        ChannelFragment fragment = ChannelFragment.newInstance();
-        ft.replace(R.id.container, fragment);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        ft.commit();
+    }
+
+    private void showChannelPlaceholder() {
+        //TODO show Main Placeholder
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        GroupChannelAdapter adapter = new GroupChannelAdapter(getSupportFragmentManager());
+        LinkedList<String> groupList = new LinkedList<>();
+        for (int i = 0; i < mPlaylist.getPlaylistItems().size(); i++) {
+            M3UItem m3UItem = mPlaylist.getPlaylistItems().get(i);
+            if (groupList.contains(m3UItem.getItemGroup())) {
+                continue;
+            }
+            groupList.add(m3UItem.getItemGroup());
+        }
+        adapter.addFragment(getString(R.string.favorites));
+        adapter.addFragment(getString(R.string.all_channels));
+        for (int i = 0; i < groupList.size(); i++) {
+            String groupTitle = groupList.get(i);
+            if (!TextUtils.isEmpty(groupTitle)) {
+                adapter.addFragment(groupTitle);
+            }
+        }
+        viewPager.setAdapter(adapter);
+    }
+
+    private void loadServer(String url) {
+        //mProgressBar.setVisibility(View.VISIBLE);
+        new LoadServer().execute(url);
     }
 
     @Override
@@ -121,4 +221,85 @@ public class MainActivity extends AppCompatActivity
     private void navigateToPlaylist() {
         Router.navigateTo(this, Router.Screens.PLAYLIST);
     }
+
+    private void requestStoragePermission() {
+        if (IseaSoft.checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE) && IseaSoft.checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            loadChannels();
+        } else {
+            if (IseaSoft.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(panelLayout, "iSeaMusic will need to read external storage to display songs on your device.",
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                IseaSoft.askForPermission(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, permissionReadstorageCallback);
+                            }
+                        }).show();
+            } else {
+                IseaSoft.askForPermission(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, permissionReadstorageCallback);
+            }
+        }
+    }
+
+    private void parseAndUpdateUI(InputStream inputStream) {
+
+        M3UParser m3UParser = new M3UParser();
+        try {
+            mPlaylist = m3UParser.parseFile(inputStream);
+            new Handler(Looper.getMainLooper()).post(() -> {
+                if (viewPager != null) {
+                    setupViewPager(viewPager);
+                }
+            });
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class GroupChannelAdapter extends FragmentStatePagerAdapter {
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public GroupChannelAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(String title) {
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ChannelFragment.newInstance(mFragmentTitles.get(position));
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentTitles.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class LoadServer extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... urls) {
+
+            HttpHandler hh = new HttpHandler();
+            InputStream inputStream = hh.makeServiceCall(urls[0]);
+
+            parseAndUpdateUI(inputStream);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
 }
--- app/src/main/java/com/iseasoft/iseaiptv/ui/fragment/ChannelFragment.java
+++ app/src/main/java/com/iseasoft/iseaiptv/ui/fragment/ChannelFragment.java
@@ -1,14 +1,8 @@
 package com.iseasoft.iseaiptv.ui.fragment;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.preference.PreferenceManager;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
@@ -20,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.SearchView;
@@ -28,22 +23,14 @@ import com.afollestad.appthemeengine.ATE;
 import com.iseasoft.iseaiptv.R;
 import com.iseasoft.iseaiptv.adapters.ChannelAdapter;
 import com.iseasoft.iseaiptv.adapters.FolderAdapter;
-import com.iseasoft.iseaiptv.helpers.Router;
-import com.iseasoft.iseaiptv.http.HttpHandler;
 import com.iseasoft.iseaiptv.listeners.FolderListener;
-import com.iseasoft.iseaiptv.models.M3UPlaylist;
-import com.iseasoft.iseaiptv.models.Playlist;
-import com.iseasoft.iseaiptv.parsers.M3UParser;
-import com.iseasoft.iseaiptv.permissions.IseaSoft;
-import com.iseasoft.iseaiptv.permissions.PermissionCallback;
-import com.iseasoft.iseaiptv.utils.PreferencesUtility;
+import com.iseasoft.iseaiptv.models.M3UItem;
+import com.iseasoft.iseaiptv.ui.activity.MainActivity;
 import com.iseasoft.iseaiptv.utils.Utils;
 import com.iseasoft.iseaiptv.widgets.DividerItemDecoration;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nv95 on 10.11.16.
@@ -52,17 +39,6 @@ import java.io.InputStream;
 public class ChannelFragment extends Fragment {
 
     private static final int COLUMN_WIDTH = 70;
-    private final PermissionCallback permissionReadstorageCallback = new PermissionCallback() {
-        @Override
-        public void permissionGranted() {
-            loadChannels();
-        }
-
-        @Override
-        public void permissionRefused() {
-
-        }
-    };
     private RelativeLayout panelLayout;
     private FolderAdapter mAdapter;
     private RecyclerView recyclerView;
@@ -70,9 +46,11 @@ public class ChannelFragment extends Fragment {
     private ChannelAdapter channelAdapter;
 
     private FolderListener listener;
+    private String groupName;
 
-    public static ChannelFragment newInstance() {
+    public static ChannelFragment newInstance(String groupName) {
         ChannelFragment fragment = new ChannelFragment();
+        fragment.groupName = groupName;
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
@@ -112,37 +90,43 @@ public class ChannelFragment extends Fragment {
             mAdapter.applyTheme(dark);
             mAdapter.notifyDataSetChanged();
         }
-
-        if (Utils.isMarshmallow()) {
-            requestStoragePermission();
-        } else {
-            loadChannels();
-        }
+        showChannels();
     }
 
-    private void loadChannels() {
-        final Playlist lastPlaylist = PreferencesUtility.getInstance(getActivity()).getLastPlaylist();
-        if (lastPlaylist != null) {
-            if (lastPlaylist.getLink().startsWith("http")) {
-                loadServer(lastPlaylist.getLink());
-            } else {
-                try {
-                    File file = new File(lastPlaylist.getLink());
-                    InputStream inputStream = new FileInputStream(file);
-                    parseAndUpdateUI(inputStream);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+    }
+
+    private void showChannels() {
+        if (channelAdapter == null) {
+            channelAdapter = new ChannelAdapter(getActivity());
+        }
+        channelAdapter.update(getPlaylistItems());
+        recyclerView.setAdapter(channelAdapter);
+        //int columnWidthInDp = COLUMN_WIDTH;
+        //int spanCount = Utils.getOptimalSpanCount(recyclerView, columnWidthInDp);
+        //Utils.modifyRecylerViewForGridView(recyclerView, spanCount, columnWidthInDp);
+        Utils.modifyListViewForVertical(getActivity(), recyclerView);
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+    private List<M3UItem> getPlaylistItems() {
+        MainActivity mainActivity = (MainActivity) getActivity();
+        List<M3UItem> allChannels = mainActivity.getPlaylist().getPlaylistItems();
+        if (groupName.equals(getString(R.string.all_channels))) {
+            return allChannels;
+        } else if (groupName.equals(getString(R.string.favorites))) {
+            //return PreferencesUtility.getInstance(getActivity()).getFavoriteChannels();
+        }
+        ArrayList<M3UItem> list = new ArrayList<>();
+        for (int i = 0; i < allChannels.size(); i++) {
+            M3UItem item = allChannels.get(i);
+            if (groupName.equals(item.getItemGroup())) {
+                list.add(item);
             }
-        } else {
-            showChannelPlaceholder();
-            Router.navigateTo(getActivity(), Router.Screens.PLAYLIST);
         }
-
-    }
-
-    private void showChannelPlaceholder() {
-
+        return list;
     }
 
     private void setItemDecoration() {
@@ -184,6 +168,15 @@ public class ChannelFragment extends Fragment {
             }
         });
 
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                setKeyboardVisibility(false);
+                showChannels();
+                return true;
+            }
+        });
+
     }
 
     @Override
@@ -210,11 +203,6 @@ public class ChannelFragment extends Fragment {
         }
     }
 
-    private void loadServer(String url) {
-        mProgressBar.setVisibility(View.VISIBLE);
-        new LoadServer().execute(url);
-    }
-
     public void updateTheme() {
         Context context = getActivity();
         if (context != null) {
@@ -223,65 +211,12 @@ public class ChannelFragment extends Fragment {
         }
     }
 
-    private void requestStoragePermission() {
-        if (IseaSoft.checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE) && IseaSoft.checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            loadChannels();
+    public void setKeyboardVisibility(boolean show) {
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (show) {
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
         } else {
-            if (IseaSoft.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                Snackbar.make(panelLayout, "iSeaMusic will need to read external storage to display songs on your device.",
-                        Snackbar.LENGTH_INDEFINITE)
-                        .setAction("OK", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                IseaSoft.askForPermission(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, permissionReadstorageCallback);
-                            }
-                        }).show();
-            } else {
-                IseaSoft.askForPermission(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, permissionReadstorageCallback);
-            }
+            imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
         }
     }
-
-    private void parseAndUpdateUI(InputStream inputStream) {
-
-        M3UParser m3UParser = new M3UParser();
-        try {
-            M3UPlaylist playlist = m3UParser.parseFile(inputStream);
-            new Handler(Looper.getMainLooper()).post(() -> {
-                if (channelAdapter == null) {
-                    channelAdapter = new ChannelAdapter(getActivity());
-                }
-                channelAdapter.update(playlist.getPlaylistItems());
-                recyclerView.setAdapter(channelAdapter);
-                //int columnWidthInDp = COLUMN_WIDTH;
-                //int spanCount = Utils.getOptimalSpanCount(recyclerView, columnWidthInDp);
-                //Utils.modifyRecylerViewForGridView(recyclerView, spanCount, columnWidthInDp);
-                Utils.modifyListViewForVertical(getActivity(), recyclerView);
-                mProgressBar.setVisibility(View.GONE);
-            });
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @SuppressLint("StaticFieldLeak")
-    private class LoadServer extends AsyncTask<String, Void, Void> {
-
-        @Override
-        protected Void doInBackground(String... urls) {
-
-            HttpHandler hh = new HttpHandler();
-            InputStream inputStream = hh.makeServiceCall(urls[0]);
-
-            parseAndUpdateUI(inputStream);
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-        }
-    }
-
-
 }
