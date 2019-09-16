7ee4168dd3608d0d3d4a6ec2139283be857a1486
uttu87 <haivan.qn@gmail.com>
2019-09-16 21:25:48 +0700

    Clone to Egypt TV app

--- app/src/main/java/com/iseasoft/iseaiptv/App.java
+++ app/src/main/java/com/iseasoft/iseaiptv/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
 
     public static int screenCount = 0;
     private static App mSelf;
-    private static String baseUrl = "https://raw.githubusercontent.com/freearhey/iptv/master/channels/gr.m3u";
+    private static String baseUrl = "https://raw.githubusercontent.com/freearhey/iptv/master/channels/eg.m3u";
     private static boolean useOnlineData = true;
     private static boolean activeAds = true;
     private static boolean useAdMob = true;
@@ -30,7 +30,7 @@ public class App extends Application {
     private static long adsType = 1;
     private static String admobBannerId = "";
     private static String admobInterstitialId = "";
-    private static String startAppId = "208302112";
+    private static String startAppId = "208521310";
 
     private static ArrayList<M3UItem> channelList = new ArrayList<>();
 
