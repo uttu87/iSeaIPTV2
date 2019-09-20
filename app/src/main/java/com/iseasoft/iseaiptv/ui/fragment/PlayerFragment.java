16f04dc5dd1d0bb09c483ac500bbdc36f06cb6f9
uttu87 <haivan.qn@gmail.com>
2019-09-21 02:05:28 +0700

    Implement watching history

--- app/src/main/java/com/iseasoft/iseaiptv/ui/fragment/PlayerFragment.java
+++ app/src/main/java/com/iseasoft/iseaiptv/ui/fragment/PlayerFragment.java
@@ -300,6 +300,7 @@ public class PlayerFragment extends BaseFragment implements OnPreparedListener, 
         mVideoController.setTitle(channel.getItemName());
         mVideoController.showPlayErrorMessage(false);
         updateFavoriteIcon();
+        PreferencesUtility.getInstance(getActivity()).addHistory(channel);
     }
 
     private void setUpVideoViewSize(boolean isFullscreen) {
