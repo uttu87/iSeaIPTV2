75b5ec28045b860f171587fd6db82fa3efbe38b4
uttu87 <haivan.qn@gmail.com>
2019-09-22 14:23:07 +0700

    Update url channels

--- app/src/main/java/com/iseasoft/iseaiptv/App.java
+++ app/src/main/java/com/iseasoft/iseaiptv/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
 
     public static int screenCount = 0;
     private static App mSelf;
-    private static String baseUrl = "https://raw.githubusercontent.com/freearhey/iptv/master/channels/cn.m3u";
+    private static String baseUrl = "https://raw.githubusercontent.com/uttu87/livetv/master/cn.m3u";
     private static boolean useOnlineData = true;
     private static boolean activeAds = true;
     private static boolean useAdMob = true;
@@ -162,7 +162,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        mSelf = this;
         ImageLoaderConfiguration localImageLoaderConfiguration = new ImageLoaderConfiguration.Builder(this).imageDownloader(new BaseImageDownloader(this) {
             PreferencesUtility prefs = PreferencesUtility.getInstance(App.this);
 
