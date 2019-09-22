6ae8abc100b90a143cb1f5ab076b20c96e6edcb9
uttu87 <haivan.qn@gmail.com>
2019-09-22 20:56:43 +0700

    Fixed empty playlist at the first launch app sometimes

--- app/src/main/java/com/iseasoft/iseaiptv/ui/activity/SplashActivity.java
+++ app/src/main/java/com/iseasoft/iseaiptv/ui/activity/SplashActivity.java
@@ -81,6 +81,7 @@ public class SplashActivity extends AppCompatActivity {
 
             @Override
             public void onError(Error e) {
+                savePlaylist();
                 navigationToMainScreen();
             }
         });
