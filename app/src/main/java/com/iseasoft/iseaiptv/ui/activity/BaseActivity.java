a4f53a203cb3f1669d57ef78d5efe9068c310d9c
uttu87 <haivan.qn@gmail.com>
2019-09-24 18:27:43 +0700

    Disable Admob

--- app/src/main/java/com/iseasoft/iseaiptv/ui/activity/BaseActivity.java
+++ app/src/main/java/com/iseasoft/iseaiptv/ui/activity/BaseActivity.java
@@ -59,9 +59,10 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         unbinder = ButterKnife.bind(this);
-        initAdmob();
+        //initAdmob();
         initStartAppSdk();
-        setupAdmob();
+        //setupAdmob();
+        setupPublisherAds();
 
     }
 
