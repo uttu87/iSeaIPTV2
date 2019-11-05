303f734129dc842015f3aef1f2bb0b7d20d05a2f
uttu87 <haivan.qn@gmail.com>
2019-11-05 20:54:47 +0700

    Add StartApp Native Ad

--- app/src/main/java/com/iseasoft/iseaiptv/ui/fragment/AdsFragment.java
+++ app/src/main/java/com/iseasoft/iseaiptv/ui/fragment/AdsFragment.java
@@ -1,12 +1,14 @@
 package com.iseasoft.iseaiptv.ui.fragment;
 
-import com.google.android.gms.ads.AdLoader;
-import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
-import com.google.android.gms.ads.formats.UnifiedNativeAd;
-import com.iseasoft.iseaiptv.R;
 import com.iseasoft.iseaiptv.adapters.AdsAdapter;
+import com.startapp.android.publish.ads.nativead.NativeAdDetails;
+import com.startapp.android.publish.ads.nativead.NativeAdPreferences;
+import com.startapp.android.publish.ads.nativead.StartAppNativeAd;
+import com.startapp.android.publish.adsCommon.Ad;
+import com.startapp.android.publish.adsCommon.adListeners.AdEventListener;
 
 import java.util.List;
+import java.util.Random;
 
 public class AdsFragment extends BaseFragment {
     protected final static int LIST_VIEW_ADS_COUNT = 10;
@@ -23,6 +25,34 @@ public class AdsFragment extends BaseFragment {
         if (isExistAds(adapter)) {
             return;
         }
+        int numberOfAds = 3;
+        final List<Object> mDataSet = adapter.getDataSet();
+        StartAppNativeAd mStartAppNativeAd = new StartAppNativeAd(getActivity());
+        mStartAppNativeAd.loadAd(
+                new NativeAdPreferences()
+                        .setAdsNumber(numberOfAds)
+                        .setAutoBitmapDownload(true)
+                        .setPrimaryImageSize(2),
+                new AdEventListener() {
+                    @Override
+                    public void onReceiveAd(Ad ad) {
+                        if (isExistAds(adapter)) {
+                            return;
+                        }
+                        for (int i = ADS_ITEM_START_INDEX; i <= mDataSet.size(); i += (spaceBetweenAds + 1)) {
+                            final int index = new Random().nextInt(mStartAppNativeAd.getNativeAds().size());
+                            adapter.getDataSet().add(i, mStartAppNativeAd.getNativeAds().get(index));
+                            adapter.notifyItemRangeChanged(i, spaceBetweenAds);
+                        }
+                    }
+
+                    @Override
+                    public void onFailedToReceiveAd(Ad ad) {
+
+                    }
+                });
+
+        /*
         AdLoader adLoader = new AdLoader.Builder(getActivity(), getString(R.string.native_ads_id))
                 .forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
                     @Override
@@ -41,11 +71,13 @@ public class AdsFragment extends BaseFragment {
 
         adLoader.loadAd(new PublisherAdRequest.Builder()
                 .addTestDevice("FB536EF8C6F97686372A2C5A5AA24BC5").build());
+
+         */
     }
 
     private boolean isExistAds(final AdsAdapter adsAdapter) {
         for (Object item : adsAdapter.getDataSet()) {
-            if (item instanceof UnifiedNativeAd) {
+            if (item instanceof NativeAdDetails) {
                 return true;
             }
         }
