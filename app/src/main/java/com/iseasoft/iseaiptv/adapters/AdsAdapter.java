27f7a3f0f7caddc3f2b8cb2720c9ed96752a6e15
uttu87 <haivan.qn@gmail.com>
2019-11-16 20:25:01 +0700

    Added NativeAdDetails

--- app/src/main/java/com/iseasoft/iseaiptv/adapters/AdsAdapter.java
+++ app/src/main/java/com/iseasoft/iseaiptv/adapters/AdsAdapter.java
@@ -16,6 +16,7 @@ import com.google.android.gms.ads.formats.UnifiedNativeAd;
 import com.google.android.gms.ads.formats.UnifiedNativeAdView;
 import com.iseasoft.iseaiptv.R;
 import com.iseasoft.iseaiptv.utils.Utils;
+import com.startapp.android.publish.ads.nativead.NativeAdDetails;
 
 import java.util.List;
 
@@ -41,8 +42,7 @@ public class AdsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
         final NativeExpressAdViewHolder nativeExpressHolder = (NativeExpressAdViewHolder) viewHolder;
-        UnifiedNativeAd unifiedNativeAd = (UnifiedNativeAd) mItems.get(i);
-        nativeExpressHolder.setContent(unifiedNativeAd);
+        nativeExpressHolder.setContent(mItems.get(i));
     }
 
     @Override
@@ -61,7 +61,7 @@ public class AdsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         // then return NATIVE_EXPRESS_AD_VIEW_TYPE otherwise DATA_VIEW_TYPE
         // By the logic defined below, an ad unit will be showed after every spaceBetweenAds numbers of data items
         Object item = mItems.get(position);
-        if (item instanceof UnifiedNativeAd) {
+        if (item instanceof UnifiedNativeAd || item instanceof NativeAdDetails) {
             return NATIVE_EXPRESS_AD_VIEW_TYPE;
         }
         return DATA_VIEW_TYPE;
@@ -85,25 +85,49 @@ public class AdsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             this.footer = view.findViewById(R.id.footer);
         }
 
-        public void setContent(UnifiedNativeAd unifiedNativeAd) {
-            templateView.setNativeAd(unifiedNativeAd);
-            title.setText(unifiedNativeAd.getHeadline());
-            artist.setText(unifiedNativeAd.getBody());
-            if (unifiedNativeAd.getIcon() != null) {
-                albumArt.setImageDrawable(unifiedNativeAd.getIcon().getDrawable());
-            } else {
-                albumArt.setBackgroundResource(R.mipmap.ic_launcher);
-            }
-            templateView.setCallToActionView(templateView);
-            Double starRating = unifiedNativeAd.getStarRating();
-            if (starRating != null && starRating > 0) {
-                ratingBar.setVisibility(VISIBLE);
-                ratingBar.setRating(starRating.floatValue());
-                ratingBar.setMax(5);
-                templateView.setStarRatingView(ratingBar);
-                artist.setVisibility(GONE);
-            } else {
-                ratingBar.setVisibility(GONE);
+        public void setContent(Object nativeAds) {
+            if (nativeAds instanceof UnifiedNativeAd) {
+                UnifiedNativeAd unifiedNativeAd = (UnifiedNativeAd) nativeAds;
+                templateView.setNativeAd(unifiedNativeAd);
+                title.setText(unifiedNativeAd.getHeadline());
+                artist.setText(unifiedNativeAd.getBody());
+                if (unifiedNativeAd.getIcon() != null) {
+                    albumArt.setImageDrawable(unifiedNativeAd.getIcon().getDrawable());
+                } else {
+                    albumArt.setBackgroundResource(R.mipmap.ic_launcher);
+                }
+                templateView.setCallToActionView(templateView);
+                Double starRating = unifiedNativeAd.getStarRating();
+                if (starRating != null && starRating > 0) {
+                    ratingBar.setVisibility(VISIBLE);
+                    ratingBar.setRating(starRating.floatValue());
+                    ratingBar.setMax(5);
+                    templateView.setStarRatingView(ratingBar);
+                    artist.setVisibility(GONE);
+                } else {
+                    ratingBar.setVisibility(GONE);
+                }
+            } else if(nativeAds instanceof NativeAdDetails) {
+                NativeAdDetails nativeAdDetails = (NativeAdDetails) nativeAds;
+                nativeAdDetails.registerViewForInteraction(templateView);
+                title.setText(nativeAdDetails.getTitle());
+                artist.setText(nativeAdDetails.getDescription());
+                if (nativeAdDetails.getImageBitmap() != null) {
+                    albumArt.setImageBitmap(nativeAdDetails.getImageBitmap());
+                } else {
+                    albumArt.setBackgroundResource(R.mipmap.ic_launcher);
+                }
+                //templateView.setCallToActionView(templateView);
+                float starRating = nativeAdDetails.getRating();
+                if (starRating > 0) {
+                    ratingBar.setVisibility(VISIBLE);
+                    ratingBar.setRating(starRating);
+                    ratingBar.setMax(5);
+                    //templateView.setStarRatingView(ratingBar);
+                    artist.setVisibility(GONE);
+                } else {
+                    ratingBar.setVisibility(GONE);
+                }
             }
 
             if (isGrid) {
--- app/src/main/java/com/iseasoft/iseaiptv/ui/fragment/AdsFragment.java
+++ app/src/main/java/com/iseasoft/iseaiptv/ui/fragment/AdsFragment.java
@@ -1,12 +1,19 @@
 package com.iseasoft.iseaiptv.ui.fragment;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 import com.google.android.gms.ads.formats.UnifiedNativeAd;
-import com.iseasoft.iseaiptv.R;
+import com.iseasoft.iseaiptv.App;
 import com.iseasoft.iseaiptv.adapters.AdsAdapter;
+import com.startapp.android.publish.ads.nativead.NativeAdDetails;
+import com.startapp.android.publish.ads.nativead.NativeAdPreferences;
+import com.startapp.android.publish.ads.nativead.StartAppNativeAd;
+import com.startapp.android.publish.adsCommon.Ad;
+import com.startapp.android.publish.adsCommon.adListeners.AdEventListener;
 
 import java.util.List;
+import java.util.Random;
 
 public class AdsFragment extends BaseFragment {
     protected final static int LIST_VIEW_ADS_COUNT = 10;
@@ -23,7 +30,39 @@ public class AdsFragment extends BaseFragment {
         if (isExistAds(adapter)) {
             return;
         }
-        AdLoader adLoader = new AdLoader.Builder(getActivity(), getString(R.string.native_ads_id))
+        AdLoader adLoader = new AdLoader.Builder(getActivity(), App.getPublisherNativeId())
+                .withAdListener(new AdListener() {
+                    @Override
+                    public void onAdFailedToLoad(int i) {
+                        super.onAdFailedToLoad(i);
+                        int numberOfAds = 3;
+                        final List<Object> mDataSet = adapter.getDataSet();
+                        StartAppNativeAd mStartAppNativeAd = new StartAppNativeAd(getActivity());
+                        mStartAppNativeAd.loadAd(
+                                new NativeAdPreferences()
+                                        .setAdsNumber(numberOfAds)
+                                        .setAutoBitmapDownload(true)
+                                        .setPrimaryImageSize(2),
+                                new AdEventListener() {
+                                    @Override
+                                    public void onReceiveAd(Ad ad) {
+                                        if (isExistAds(adapter)) {
+                                            return;
+                                        }
+                                        for (int i = ADS_ITEM_START_INDEX; i <= mDataSet.size(); i += (spaceBetweenAds + 1)) {
+                                            final int index = new Random().nextInt(mStartAppNativeAd.getNativeAds().size());
+                                            adapter.getDataSet().add(i, mStartAppNativeAd.getNativeAds().get(index));
+                                            adapter.notifyItemRangeChanged(i, spaceBetweenAds);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onFailedToReceiveAd(Ad ad) {
+
+                                    }
+                                });
+                    }
+                })
                 .forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
                     @Override
                     public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
@@ -45,7 +84,7 @@ public class AdsFragment extends BaseFragment {
 
     private boolean isExistAds(final AdsAdapter adsAdapter) {
         for (Object item : adsAdapter.getDataSet()) {
-            if (item instanceof UnifiedNativeAd) {
+            if (item instanceof UnifiedNativeAd || item instanceof NativeAdDetails) {
                 return true;
             }
         }
